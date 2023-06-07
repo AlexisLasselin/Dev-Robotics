3b97c10db84a671283c05f05e5a3f107b78318d2
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2023-06-07 17:50:11 +0200

    Advent of Code 2022 part 1 made

---
+++ AdventOfCode/2022_Day1.py
@@ -0,0 +1,22 @@
+# Part 1:
+#  Objective: Find the biggest sum of calories for an elf in the file
+
+with open("2022_Day1_Input.txt") as f:
+	lines = f.readlines()
+    
+	calories = [entry.strip() for entry in lines]
+
+elf_sums = []
+current_sum = 0
+for entry in calories:
+    if entry != '':
+        current_sum += int(entry)
+    elif entry == '':
+        elf_sums.append(current_sum)
+        current_sum = 0
+elf_sums.append(current_sum)
+
+print(max(elf_sums))
+
+# Part 2:
+# How many Calories are the top 3 Elves carrying in total?
